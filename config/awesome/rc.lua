073d72d4e918d64d344cec531a749a4a9ee29faf
Ritchie <ritchie@macapinlac.com>
2022-10-21 06:31:07 -0700

    Temp revert back to regular terminal

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -52,8 +52,8 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
--- terminal = "x-terminal-emulator"
-terminal = "/usr/local/bin/alacritty"
+terminal = "x-terminal-emulator"
+-- terminal = "/usr/local/bin/alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
