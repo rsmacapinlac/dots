7b8902e2efaa82afb0907c31bd0cd001ef516bd3
Ritchie <ritchie@macapinlac.com>
2022-10-30 10:55:27 -0700

    Updated AwesomeWM config with polybar

---
+++ screenlayout/docked-home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP-1-4 --off
