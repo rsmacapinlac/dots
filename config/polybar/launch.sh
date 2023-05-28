6c253a3f8cd9079027ea852f08a987223e1a58d6
Ritchie <ritchie@macapinlac.com>
2023-05-28 11:35:19 -0700

    Updated polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,6 +3,7 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar main &
+polybar top &
 
 #if type "xrandr"; then
 #  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
