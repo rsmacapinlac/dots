c4cc9fb6aed51433aae431c9e6475b4ef81eafb4
Ritchie <ritchie@macapinlac.com>
2025-03-20 08:24:11 -0700

    Incorporated Mason and lsp config

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -12,13 +12,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
+
+-- incorporate plugins below
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}
+  { "nvim-treesitter/nvim-treesitter", build= ":TSUpdate" },
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
+  
 }
 
 require("lazy").setup(plugins, opts)
