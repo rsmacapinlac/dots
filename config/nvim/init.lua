a052cadda75490e5d9f63f1639b9113fc09c036a
Ritchie <ritchie@macapinlac.com>
2025-03-21 09:13:02 -0700

    Updated nvim config with lualine and obsidian

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,9 @@ require("core.remap");
 require("core.settings");
 require("core.plugins");
 
+require("core.plugins_config.lualine");
 require("core.plugins_config.catppuccin");
 require("core.plugins_config.telescope");
 require("core.plugins_config.treesitter");
 require("core.plugins_config.mason");
+require("core.plugins_config.obsidian");
