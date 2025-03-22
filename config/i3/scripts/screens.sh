26da88f24242f03e50a315a5357a204ad9f2c2f6
Ritchie <ritchie@macapinlac.com>
2025-03-21 21:03:43 -0700

    Starting from scratch, removing all the unnecessary stuff

--- config/i3/scripts/screens.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-xrandr --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 1920x1080 --pos 640x0 --rotate normal --output HDMI-2 --off --output DP-3 --off --output HDMI-3 --primary --mode 2560x1080 --pos 0x1080 --rotate normal
