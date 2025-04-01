a18b14937572fb058ed052871b1b82966fb81227
Ritchie <ritchie@macapinlac.com>
2025-04-01 10:35:34 -0700

    Update to check if the Obsidian vault folder exists

--- config/nvim/lua/core/plugins_config/obsidian.lua
+++ config/nvim/lua/core/plugins_config/obsidian.lua
@@ -1,9 +1,16 @@
-require("obsidian").setup({
-  workspaces = {
-    {
-      name = "Second Brain",
-      path = "~/Nextcloud/Apps/obsidian/Second Brain",
+local function folder_exist(path)
+  local attr = io.popen('ls -ld "' ..path ..'" 2>/dev/null'):read("*a")
+  return attr:match("^d")
+end
+
+if folder_exist("~/Nextcloud/Apps/Obsidian/Second Brain") then
+  require("obsidian").setup({
+    workspaces = {
+      {
+        name = "Second Brain",
+        path = "~/Nextcloud/Apps/obsidian/Second Brain",
+      },
     },
-  },
-  -- see below for full list of options 👇
-})
+    -- see below for full list of options 👇
+  })
+end
