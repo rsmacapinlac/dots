e1d78b6fffd975c11a9bf66488bc57ef7852cf92
Ritchie <ritchie@macapinlac.com>
2025-04-12 19:48:30 -0700

    Updating navigation keybindings and wallpaper logic

--- config/awesome/deco/wallpaper.lua
+++ config/awesome/deco/wallpaper.lua
@@ -1,19 +1,12 @@
 -- Standard awesome library
 local gears = require("gears")
 local beautiful = require("beautiful")
+local awful = require("awful")
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 function set_wallpaper(s)
-  -- Wallpaper
-  if beautiful.wallpaper then
-    local wallpaper = beautiful.wallpaper
-    -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
-      wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
-  end
+  awful.util.spawn(RC.vars.wallpaper_cmd)
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
