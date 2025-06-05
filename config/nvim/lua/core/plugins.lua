69f48505ff2ae6c572b1895a67612be3eecf0d83
Ritchie <ritchie@macapinlac.com>
2025-06-05 06:36:30 -0700

    Zen mode

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -36,8 +36,15 @@ local plugins = {
     lazy = true,
     ft = "markdown",
     dependecies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   }
-  
 }
 
 require("lazy").setup(plugins, opts)
