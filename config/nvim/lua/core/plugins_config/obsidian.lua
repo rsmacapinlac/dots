cf0a730ecf4f4ea9906278dd1f8958a093e371c6
Ritchie <ritchie@macapinlac.com>
2025-07-22 16:34:09 -0700

    Clean up to make the Obisidian plugin work

--- config/nvim/lua/core/plugins_config/obsidian.lua
+++ config/nvim/lua/core/plugins_config/obsidian.lua
@@ -1,16 +1,16 @@
-local function folder_exist(path)
-  local attr = io.popen('ls -ld "' ..path ..'" 2>/dev/null'):read("*a")
-  return attr:match("^d")
-end
-
-if folder_exist("~/Second Brain") then
-  require("obsidian").setup({
-    workspaces = {
-      {
-        name = "Second Brain",
-        path = "~/Second Brain",
-      },
+require("obsidian").setup({
+  workspaces = {
+    {
+      name = "Second Brain",
+      path = "~/Second Brain",
     },
-    -- see below for full list of options 👇
-  })
-end
+  },
+  opts = {
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+  },
+})
