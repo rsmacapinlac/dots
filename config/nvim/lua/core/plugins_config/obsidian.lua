73d213b2314d9e5710150ee7b367cd325f6cce24
Ritchie <ritchie@macapinlac.com>
2025-07-22 17:32:24 -0700

    Updated obsidian config

--- config/nvim/lua/core/plugins_config/obsidian.lua
+++ config/nvim/lua/core/plugins_config/obsidian.lua
@@ -6,6 +6,24 @@ require("obsidian").setup({
     },
   },
   opts = {
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
     completion = {
       -- Set to false to disable completion.
       nvim_cmp = true,
